5a23fab8fb754835077934af9d81e9ffad4cadbf
MH-Shakil38 <maynuddinhsn@gmail.com>
2023-09-21 22:05:24 +0600

    header fb shere image

--- resources/views/admin/layouts/partials/_header.blade.php
+++ resources/views/admin/layouts/partials/_header.blade.php
@@ -3,6 +3,14 @@
 
 <!-- Favicon -->
 <link rel="shortcut icon" id="favicon" type="image/x-icon" href="https://imadm.edu.bd/maynuddin.jpg">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:title" content="Your Website Title">
+<meta property="og:description" content="Description of your website">
+<meta property="og:image" content="https://maynuddinhasan.com/assets/common/img/cover/gEDKUssR5A_1695311494.jpg">
+<meta property="og:image:secure_url" content="https://maynuddinhasan.com/assets/common/img/cover/gEDKUssR5A_1695311494.jpg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:url" content="https://www.maynuddinhasan.com">
 <title>{{Config::get('app.name')}}</title>
 <link href="{{ asset('assets/common/lib/fontawesome/css/all.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/common/lib/mdi-icon/css/materialdesignicons.min.css') }}" rel="stylesheet" />
